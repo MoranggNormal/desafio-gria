5ebeb94aeddc1e419f7910558666486f0a42ff4a
eullerpeixoto <eullerz01@hotmail.com>
2021-06-26 17:30:52 -0300

    Projeto finalizado.

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,68 @@
+import { useState } from 'react'
 import Input from '../components/Input'
 import Button from '../components/Button'
 import GriaLogo from '../assets/images/logo.png'
+import LeftArrow from '../assets/images/left-arrow.svg'
+import Foto from '../assets/images/cadastro.png'
 
 import '../styles/SignUp.scss'
 
 const SignUp = () => {
+
+  const [email, setEmail] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [validateCpf, setValidateCpf] = useState(false);
+  const [pass, setPass] = useState('');
+  const [confirmPass, setConfirmPass] = useState(false);
+  const [validPass, setValidPass] = useState(false);
+
+  const [modal, setModal] = useState(false);
+
+  function cpfIsValid(cpf){
+    if(cpf.length < 11){
+       return setValidateCpf(false)
+
+    }
+    setValidateCpf(true)
+
+  }
+
+  function validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    if(!re.test(email)){
+       setEmail(false)
+       return
+    }
+    setEmail(true)
+  }
+
+  function verifyPass(){
+    if(pass !== confirmPass){
+      setValidPass(false)
+      return
+    }
+    setValidPass(true)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+  }
+
   return (
+    <>
+    <span className="prev-page">
+        <img src={LeftArrow} alt="Retornar à última página" />
+        <h5>Retornar</h5>
+    </span>
+
+
+    <div className="is-desktop">
+
+    <img src={Foto} alt="Cadastre-se na Gria" />
+
+    </div>
+
+
     <div className="bg-form">
 
       <div>
@@ -18,26 +75,40 @@ const SignUp = () => {
         </span>
       </div>
 
-      <form>
+      <form onSubmit={handleSubmit}>
       <Input
         type="text"
         placeholder="CPF"
+        onChange={e => setCpf(e.target.value)}
+        onBlur={() => cpfIsValid(cpf)}
+        className={validateCpf ? "is-valid" : "is-invalid"}
       />
       <Input
-        type="text"
+        type="email"
         placeholder="E-mail"
+        id="email"
+        onChange={e => setEmail(e.target.value)}
+        onBlur={() => validateEmail(email)}
+        className={email ? "is-valid" : "is-invalid"}
       />
       <Input
         type="password"
         placeholder="Senha"
+        onChange={e => setPass(e.target.value)}
+        className={!validPass && "is-invalid"}
       />
       <Input
         type="password"
         placeholder="Confirmar senha"
+        onChange={e => setConfirmPass(e.target.value)}
+        onBlur={verifyPass}
+        className={!validPass && "is-invalid"}
       />
 
       <Button
-      type="submit">CADASTRAR</Button>
+      type="submit"
+      disabled={(!email || !validateCpf) ? "true" : "false"}
+      >CADASTRAR</Button>
 
       <Button
       type="button"
@@ -46,6 +117,8 @@ const SignUp = () => {
       </form>
 
      </div>
+
+     </>
   )
 }
 
